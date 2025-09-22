0649ad7fea1e7b4ef6c2e4477722c7db6e37874a
silviojpa <https://github.com/silviojpa>
2025-09-22 00:48:18 -0300

    fazendo um novo commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # Retorna uma mensagem simples
-    return "Hello, DevOps World! A aplicacao esta funcionando e esta UP."
+    return "Hello, DevOps World! A aplicacao esta funcionando e esta up."
 
 if __name__ == '__main.0.0.0':
     # Usamos uma porta comum para exemplos locais
